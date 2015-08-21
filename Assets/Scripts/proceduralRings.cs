c71789670b0facb044999a95e25a12e8c907e5b4
ryosaito420 <ryocsaito@gmail.com>
2015-08-20 17:39:18 -0700

    Added basic procedural cube genreation in a 100 x 100 x 100 square

---
+++ Assets/Scripts/proceduralRings.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class proceduralRings : MonoBehaviour {
+
+	public int min = -500;
+	public int max = 500;
+	public GameObject ring;
+	public float  numberOfCubes = 1000;
+
+	Vector3 GeneratedPosition()
+	{
+		int x,y,z;
+		x = Random.Range(min,max);
+		y = Random.Range(min,max);
+		z = Random.Range(min,max);
+		return new Vector3(x,y,z);
+	}
+
+	void PlaceCubes()
+	{
+		for(int i = 0; i < numberOfCubes; i++)
+		{
+			Instantiate(ring, GeneratedPosition(), Quaternion.identity);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		PlaceCubes ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
