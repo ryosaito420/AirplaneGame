7f2c5effd50c87e6f6421d39640482676c6b9f87
ryosaito420 <ryocsaito@gmail.com>
2015-08-23 10:00:30 -0700

    Missle created w/ Missle Lock on code works
    Movement needs refinement

---
+++ Assets/missleHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missleHandler : MonoBehaviour {
+
+	public GameObject player;
+	public float speed;
+
+	private Vector3 dist = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		dist = player.transform.position - transform.position;
+		dist = dist.normalized;
+		GetComponent<Rigidbody> ().AddForce (dist * speed);
+	}
+}
